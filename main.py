3db204762a379d3ec36bd9cd17faa38e01b69fd3
L4K6 <je.pavel@seznam.cz>
2025-10-27 17:48:19 +0100

    added the ability for ai agent to use functions and make that choise himself

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from config import *
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_f import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file,write_file
 
 def main():
     load_dotenv()
@@ -45,9 +45,48 @@ def main():
         return response.text
 
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-        
+        function_call_result = call_function(function_call_part, verbose)
 
+        resp = function_call_result.parts[0].function_response.response
+        if resp is None:
+            raise RuntimeError("Missing function_response.response in tool content")
+
+        if verbose:
+            print(f"-> {resp}")
+    
+        
+def call_function(function_call_part, verbose=False):
+    name = function_call_part.name
+    args = dict(function_call_part.args or {})
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:    
+        print(f" - Calling function: {function_call_part.name}")
+    dict_of_functions = {"get_file_content": get_file_content, "get_files_info": get_files_info, "run_python_file": run_python_file, "write_file": write_file}
+    
+    if name not in dict_of_functions:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=name,
+                    response={"error": f"Unknown function: {name}"},
+                )
+            ],
+        )
+
+    kwargs = {"working_directory": "./calculator", **args}
+    result = dict_of_functions[name](**kwargs)
+    
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_call_part.name,
+                response={"result": result},
+            )
+        ],
+    )
 
 if __name__ == "__main__":
     main()
